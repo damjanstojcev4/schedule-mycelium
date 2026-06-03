d45a29eae2abb567f5936975963d33b27892885a
Damjan Stojchev <damjan.stojcev4@gmail.com>
2026-06-03 17:05:05 +0200

    Getting Started

---
+++ scheduler-mycelium/src/main/java/com/damjan/scheduler_mycelium/SchedulerMyceliumApplication.java
@@ -0,0 +1,13 @@
+package com.damjan.scheduler_mycelium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerMyceliumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerMyceliumApplication.class, args);
+	}
+
+}
---
+++ scheduler-mycelium/src/test/java/com/damjan/scheduler_mycelium/SchedulerMyceliumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damjan.scheduler_mycelium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerMyceliumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
